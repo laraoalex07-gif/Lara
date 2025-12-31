501c5c16d94b424fe8646ab16de5924a693f2c80
laraoalex07-gif <laraoalex07@gmail.com>
2025-12-31 17:12:17 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,3 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('lara-store').then((cache) => cache.addAll([
-      'index.html',
-      'aa.jpg'
-    ]))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
